031bfa20f8e3d7edac4d7be8640a7599cd0a1e7b
Francis J <fajoaq@gmail.com>
2022-04-27 21:34:54 -0400

    moved form sanitization to server side

--- pages/api/send-message.js
+++ pages/api/send-message.js
@@ -1,15 +1,18 @@
 //const axios = require("axios");
 //const qs = require("qs");
 import { validate } from "../../src/components/forms/utils/validate.utils";
+import { sanitizeFormValues } from "../../src/components/forms/utils/sanitize-form.utils";
 import { createFormSchema } from "../../src/components/forms/utils/create-form-schema.utils";
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   // Dummy Api Code
-  if (req.body) return res.status(200).json({ message: "Message Sent" });
+  const sanitizedValues = await sanitizeFormValues(req.body);
+
+  if (sanitizedValues) return res.status(200).json({ message: "Message Sent" });
 
   return res.status(500).end();
 };
 
-// The validate middleware can take in an object, or a function
-// that returns a promise, for its first argument
+// The validate middleware can take in an object for its first argument
+// or a function that returns a promise
 export default validate(createFormSchema, handler);
