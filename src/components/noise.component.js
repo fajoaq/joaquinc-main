6a3d7952adcc0e0898d826489ce43e651e00838c
Francis J <fajoaq@gmail.com>
2022-05-10 17:03:18 -0400

    attempt fix mobile scrolling performance

--- src/components/noise.component.js
+++ src/components/noise.component.js
@@ -1,20 +1,42 @@
 import { styled } from "@mui/material/styles";
 
+const StyledContainer = styled("div")`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  pointer-events: none;
+`;
+
 // The component uses a PNG and a SVG layered on top
 // the SVG has turbulence that targets #noiseFilter, and filter accentuates this effect
 const StyledDiv = styled("div")`
-  position: absolute;
+  overflow: hidden; // added for pseudo-element
+  position: relative; // added for pseudo-element
   height: 100%;
   width: 100%;
   opacity: 0.16;
   z-index: -10;
-  background: url(/static/site/grain.png), url(/static/site/bg-turbulence.svg);
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
+
   filter: contrast(170%) brightness(170%);
+
+  // Fixed-position background image
+  &::before {
+    content: "";
+    position: fixed; // instead of background-attachment
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    background: url(/static/site/grain.png),
+      url(/static/site/bg-turbulence.svg) no-repeat;
+    background-size: cover;
+    will-change: transform; // creates a new paint layer
+  }
 `;
 
-const Noise = () => <StyledDiv id="noiseFilter" role="presentation" />;
+const Noise = () => (
+  <StyledContainer role="presentation">
+    <StyledDiv id="noiseFilter" />
+  </StyledContainer>
+);
 
 export { Noise };
